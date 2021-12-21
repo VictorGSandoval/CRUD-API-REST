acfd7b2ef0397dcfb1a183fe94113a9f9fd61a3d
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2021-12-21 18:02:00 -0500

    List Frutas and add frontend html

---
+++ src/main/java/com/govajava/gova/GovaApplication.java
@@ -0,0 +1,13 @@
+package com.govajava.gova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govajava/gova/GovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govajava.gova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
