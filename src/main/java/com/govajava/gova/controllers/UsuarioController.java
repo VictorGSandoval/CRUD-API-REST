f864772efabd63a58294f60facf4cf20d9a5d894
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2021-12-21 18:24:09 -0500

    Get User Static

--- src/main/java/com/govajava/gova/controllers/UsuarioController.java
+++ src/main/java/com/govajava/gova/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.govajava.gova.controllers;
 
 
+import com.govajava.gova.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public List<String> prueba(){
-        return List.of("Manzana","Kiwi","Banana");
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasvoy@gmail.com");
+        usuario.setTelefono("123456789");
+        return usuario;
     }
 }
